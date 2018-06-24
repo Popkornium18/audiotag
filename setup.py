15b3ff68fd50c09d33bd283ef9d5416d5a96380e
Popkornium18 <mail@popkornium18.de>
2018-06-25 00:46:00 +0200

    changed setup.py to follow the rules on packaging.python.org

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
-"""Package setup for pytagger."""
-from setuptools import setup
+import setuptools
 
-setup(
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='pytagger',
-    packages=['pytagger'],
+    version='0.0.1',
     entry_points={
         'console_scripts': ['pytagger=pytagger.pytagger:main']
     },
-    version='0.1.0',
-    description='A simple CLI audio tagger.',
     author='Simon Rose',
     author_email='mail@popkornium18.de',
+    description='A simple CLI audio tagger.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://gitlab.com/Popkornium18/pytagger',
-    keywords=['audio', 'tag', 'taglib', 'mp3', 'flac', 'ogg']
+    packages=setuptools.find_packages(),
+    keywords=['audio', 'tag', 'taglib', 'mp3', 'flac', 'ogg'],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
 )
