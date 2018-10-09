90563a328e33703c6f6e413f83b7595211ff8990
Popkornium18 <mail@popkornium18.de>
2018-10-10 00:15:41 +0200

    implemented set mode, docopt fixes

--- audiotag/audiotag.py
+++ audiotag/audiotag.py
@@ -1,30 +1,38 @@
 """
 
 Usage:
-  audiotag interactive <FILE>...
-  audiotag print <FILE>...
-  audiotag clean <FILE>...
-  audiotag rename [-p PATTERN] <FILE>...
-  audiotag (-h | --help)
-  audiotag (-v | --version)
+  audiotag print FILE...
+  audiotag interactive FILE...
+  audiotag set [--artist=ARTIST|--noartist] [--title=TITLE|--notitle]
+               [--album=ALBUM|--noalbum] [--date=DATE|--nodate]
+               [--genre=GENRE|--nogenre]
+               [--tracknumber=TRACKNUMBER|--notracknumber]
+               [--tracktotal=TRACKTOTAL|--notracktotal]
+               [--discnumber=DISCNUMBER|--nodiscnumber]
+               [--disctotal=DISCTOTAL|--nodisctotal] FILE...
+  audiotag clean FILE...
+  audiotag rename [-p PATTERN] FILE...
+  audiotag -h | --help
+  audiotag -v | --version
 
 Arguments:
     FILE    List of files to work with
     PATTERN  Formatting string for renaming files
             May contain the following tags
-                {L}  Album
-                {R}  Artist
-                {G}  Genre
+                {A}  Artist
                 {T}  Title
-                {N}  Track
+                {L}  Album
+                {Y}  Date
+                {G}  Genre
+                {N}  Tracknumber
                 {D}  Discnumber
-                {Y}  Year
+                {NT}  Tracktotal
+                {DT}  Disctotal
             Defaults to '{N} - {T}' or '{D}-{N} - {T}' (if {D} is > 1)
 
 Options:
   -h --help     Show this screen.
   -v --version  Show version.
-  -p --pattern   Specify a rename pattern
 
 """
 from docopt import docopt
@@ -32,19 +40,6 @@ import math
 import os
 import sys
 import taglib
-
-args = docopt(__doc__, version='audiotag 0.0.2')
-
-
-# Removes all tags from the file, except the 'ENCODER' tag
-def clean_mode(tracklist):
-    for track in tracklist:
-        try:
-            new_tags = {'ENCODER': track.tags['ENCODER']}
-            track.tags = new_tags
-        except KeyError:
-            track.tags = {}
-        track.save()
 
 
 def print_mode(tracklist):
@@ -59,7 +54,7 @@ def interactive_mode(tracklist):
     artist = input('Artist: ')
     album = input('Albumtitle: ')
     genre = input('Genre: ')
-    date = input('Year: ')
+    date = input('Date: ')
     tracktotal = input('Number of songs: ')
     disctotal = input('Number of discs: ')
     if disctotal == '1':
@@ -72,17 +67,49 @@ def interactive_mode(tracklist):
         print(track.path)
         title = input('Title: ')
         # Reuse the stuff we asked the user before
-        track.tags['ALBUM'] = [album]
         track.tags['ARTIST'] = [artist]
-        track.tags['GENRE'] = [genre]
-        track.tags['DATE'] = [date]
-        track.tags['DISCNUMBER'] = [discnumber]
-        track.tags['DISCTOTAL'] = [disctotal]
-        track.tags['TRACKNUMBER'] = [str(tracknumber)]
-        track.tags['TRACKTOTAL'] = [str(tracktotal)]
         track.tags['TITLE'] = [title]
+        track.tags['ALBUM'] = [album]
+        track.tags['DATE'] = [date]
+        track.tags['GENRE'] = [genre]
+        track.tags['TRACKNUMBER'] = [str(tracknumber)]
+        track.tags['DISCNUMBER'] = [discnumber]
+        track.tags['TRACKTOTAL'] = [str(tracktotal)]
+        track.tags['DISCTOTAL'] = [disctotal]
     # Loop again so the program can be safely aborted still while getting input
     for track in tracklist:
+        track.save()
+
+
+def set_mode(tracklist):
+    tags = ['ALBUM', 'ARTIST', 'GENRE', 'DATE', 'DISCNUMBER', 'DISCTOTAL',
+            'TRACKNUMBER', 'TRACKTOTAL', 'TITLE']
+    for track in tracklist:
+        modified = False
+        for tag in tags:
+            positive_opt = "--{}".format(tag.lower())
+            negative_opt = "--no{}".format(tag.lower())
+            if args[negative_opt]:
+                try:
+                    track.tags.pop(tag)
+                    modified = True
+                except KeyError:
+                    pass
+            if args[positive_opt]:
+                track.tags[tag] = [args[positive_opt]]
+                modified = True
+        if modified:
+            track.save()
+
+
+def clean_mode(tracklist):
+    for track in tracklist:
+        try:
+            # Keep ENCODER tag if it exists
+            new_tags = {'ENCODER': track.tags['ENCODER']}
+            track.tags = new_tags
+        except KeyError:
+            track.tags = {}
         track.save()
 
 
@@ -97,13 +124,15 @@ def rename_mode(tracklist):
             # Load everything that can be used to rename a file in a map
             # and replace '/' to prevent errors
             # Multiple tags are not supported, therefore [0] is used
-            filename_map['L'] = track.tags['ALBUM'][0].replace('/', '-')
             filename_map['A'] = track.tags['ARTIST'][0].replace('/', '-')
-            filename_map['G'] = track.tags['GENRE'][0].replace('/', '-')
             filename_map['T'] = track.tags['TITLE'][0].replace('/', '-')
+            filename_map['L'] = track.tags['ALBUM'][0].replace('/', '-')
+            filename_map['Y'] = track.tags['DATE'][0]
+            filename_map['G'] = track.tags['GENRE'][0].replace('/', '-')
             filename_map['N'] = track.tags['TRACKNUMBER'][0].zfill(padding)
             filename_map['D'] = track.tags['DISCNUMBER'][0]
-            filename_map['Y'] = track.tags['DATE'][0]
+            filename_map['NO'] = track.tags['TRACKTOTAL'][0]
+            filename_map['DO'] = track.tags['DISCTOTAL'][0]
             # Create the new filename, without path or file extension
             new_basename = args['PATTERN'].format(**filename_map)
             # Returns ('/path/to', 'file.flac')
@@ -119,7 +148,7 @@ def rename_mode(tracklist):
 
 def open_files():
     tracklist = []
-    for filename in args['<FILE>']:
+    for filename in args['FILE']:
         filename = os.path.abspath(filename)
         try:
             track = taglib.File(filename)
@@ -128,6 +157,7 @@ def open_files():
             print("Unable to open file '{0}'".format(filename))
     # Exit audiotag if no tracks could be opened
     if not tracklist:
+        print("No files could be opened")
         sys.exit(1)
     return tracklist
 
@@ -136,9 +166,16 @@ def main():
     tracklist = open_files()
     if args['print']:
         print_mode(tracklist)
+    elif args['set']:
+        set_mode(tracklist)
     elif args['clean']:
         clean_mode(tracklist)
     elif args['interactive']:
         interactive_mode(tracklist)
     elif args['rename']:
         rename_mode(tracklist)
+
+
+if __name__ == "__main__":
+    args = docopt(__doc__, version='audiotag 0.0.2')
+    main()
