a6f5c8b77deeb51f373ea089ea6556cede59c61e
Popkornium18 <mail@popkornium18.de>
2019-07-17 15:57:41 +0200

    Copy Mode
    Added a copy mode which allows to copy audio tags from the files in
    a source-folder to the files in a destination folder.
    
    Also some linting with pylint.

--- audiotag/audiotag.py
+++ audiotag/audiotag.py
@@ -1,16 +1,15 @@
-"""
+'''
 Usage:
   audiotag print FILE...
   audiotag interactive FILE...
   audiotag set [--artist=ARTIST|--noartist] [--title=TITLE|--notitle]
                [--album=ALBUM|--noalbum] [--date=DATE|--nodate]
-               [--genre=GENRE|--nogenre]
-               [--tracknumber=TRACKNUMBER|--notracknumber]
-               [--tracktotal=TRACKTOTAL|--notracktotal]
-               [--discnumber=DISCNUMBER|--nodiscnumber]
+               [--genre=GENRE|--nogenre] [--tracknumber=TRACKNUMBER|--notracknumber]
+               [--tracktotal=TRACKTOTAL|--notracktotal] [--discnumber=DISCNUMBER|--nodiscnumber]
                [--disctotal=DISCTOTAL|--nodisctotal] FILE...
   audiotag clean FILE...
   audiotag rename [--pattern=PATTERN] [-f] FILE...
+  audiotag copy SOURCEFOLDER DESTFOLDER
   audiotag -h | --help
   audiotag -v | --version
 
@@ -28,29 +27,31 @@ Arguments:
                  {NT}  Tracktotal
                  {DT}  Disctotal
              Defaults to '{N} - {T}' or '{D}-{N} - {T}' (if {D} is > 1)
+    SOURCE   Load files from this directory when copying tags
+    DEST     Save the tags to the files in this directory
 
 Options:
   -f --force    Overwrite existing files without confirmations
   -h --help     Show this screen.
   -v --version  Show version.
-"""
+'''
 
 
-from docopt import docopt
 import math
-import os
 import sys
+import os
+from docopt import docopt
 import taglib
 try:
-    import readline #NOQA
+    import readline #pylint: disable=unused-import
 except ImportError:
-    print("Module 'readline' not found")
+    print('Module \'readline\' not found')
 
-args = docopt(__doc__, version='audiotag 0.2.1')
-tracklist = []
-
+ARGS = docopt(__doc__, version='audiotag 0.2.1')
 
 def print_mode():
+    '''Prints all filenames and their tags and correspondig values.'''
+    tracklist = _open_files(ARGS['FILE'])
     for track in tracklist:
         print('Filename: {0}'.format(track.path))
         for tag, value in track.tags.items():
@@ -59,6 +60,7 @@ def print_mode():
 
 
 def interactive_mode():
+    tracklist = _open_files(ARGS['FILE'])
     artist = input('Artist: ')
     album = input('Albumtitle: ')
     genre = input('Genre: ')
@@ -90,27 +92,30 @@ def interactive_mode():
 
 
 def set_mode():
-    tags = ['ALBUM', 'ARTIST', 'GENRE', 'DATE', 'DISCNUMBER', 'DISCTOTAL',
-            'TRACKNUMBER', 'TRACKTOTAL', 'TITLE']
+    tracklist = _open_files(ARGS['FILE'])
+    tags = ['ALBUM', 'ARTIST', 'GENRE', 'DATE', 'DISCNUMBER', 'DISCTOTAL', 'TRACKNUMBER',
+            'TRACKTOTAL', 'TITLE']
     for track in tracklist:
         modified = False
         for tag in tags:
-            positive_opt = "--{}".format(tag.lower())
-            negative_opt = "--no{}".format(tag.lower())
-            if args[negative_opt]:
+            positive_opt = '--{}'.format(tag.lower())
+            negative_opt = '--no{}'.format(tag.lower())
+            if ARGS[negative_opt]:
                 try:
                     track.tags.pop(tag)
                     modified = True
                 except KeyError:
                     pass
-            if args[positive_opt]:
-                track.tags[tag] = [args[positive_opt]]
+            if ARGS[positive_opt]:
+                track.tags[tag] = [ARGS[positive_opt]]
                 modified = True
         if modified:
             track.save()
 
 
 def clean_mode():
+    '''Removes all tags from the files except the ENCODER tag (if it exists).'''
+    tracklist = _open_files(ARGS['FILE'])
     for track in tracklist:
         try:
             # Keep ENCODER tag if it exists
@@ -121,11 +126,32 @@ def clean_mode():
         track.save()
 
 
+def copy_mode():
+    srcfiles = _open_files(_list_files(ARGS['SOURCEFOLDER']))
+    dstfiles = _open_files(_list_files(ARGS['DESTFOLDER']))
+    srcfiles = sorted(srcfiles, key=lambda x: x.path)
+    dstfiles = sorted(dstfiles, key=lambda x: x.path)
+    if len(srcfiles) != len(dstfiles):
+        print('Different number of files in SOURCEFOLDER and DESTFOLDER. Exiting.')
+        sys.exit(1)
+    for srcfile, dstfile in zip(srcfiles, dstfiles):
+        try:
+            # srcfile is never saved so this change is not persistent, just simplifies copying
+            srcfile.tags['ENCODER'] = dstfile.tags['ENCODER']
+        except KeyError:
+            # Encoder not present in dstfile, so delete it before copying
+            del srcfile.tags['ENCODER']
+        dstfile.tags = srcfile.tags
+        dstfile.save()
+
+
 def rename_mode():
+    tracklist = _open_files(ARGS['FILE'])
+    tracklist_length = len(tracklist)
     for track in tracklist:
-        pattern = get_pattern(track)
+        pattern = _get_pattern(track)
         # Map all tags to their keys for easier renaming
-        filename_map = get_filename_map(track)
+        filename_map = _get_filename_map(track, tracklist_length)
         # Create the new filename, without path or file extension
         new_basename = pattern.format(**filename_map)
         # Returns ('/path/to', 'file.flac')
@@ -136,54 +162,56 @@ def rename_mode():
         new_name = '{0}/{1}{2}'.format(path, new_basename, extension)
         if old_name == new_name:
             continue
-        if os.path.isfile(new_name) and not args['--force']:
-            question = """File '{0}' already exists.
-Overwrite it? (y/n): """.format(new_name)
-            if not yes_no(question):
+        if os.path.isfile(new_name) and not ARGS['--force']:
+            question = '''File '{0}' already exists.\nOverwrite it? (y/n): '''.format(new_name)
+            if not _yes_no(question):
                 continue
         os.rename(old_name, new_name)
 
 
-def get_filename_map(track):
+def _get_filename_map(track, tracklist_length):
     filename_map = {}
     keys = ['A', 'T', 'L', 'Y', 'G', 'N', 'D', 'NO', 'DO']
-    tagnames = ['ARTIST', 'TITLE', 'ALBUM', 'DATE', 'GENRE', 'TRACKNUMBER',
-                'DISCNUMBER', 'TRACKTOTAL', 'DISCTOTAL']
+    tagnames = ['ARTIST', 'TITLE', 'ALBUM', 'DATE', 'GENRE', 'TRACKNUMBER', 'DISCNUMBER',
+                'TRACKTOTAL', 'DISCTOTAL']
     try:
         # Calculate number of leading zeros from 'TRACKTOTAL' tag
         padding = int(math.log10(int(track.tags['TRACKTOTAL'][0]))) + 1
     except KeyError as err:
-        print("Warning: '{0}' is missing tag {1}".format(track.path, err))
-        print("Guessing number of leading zeros from tracklist")
-        # Fallback to tracklist length if 'TRACKTOTAL' is missing
-        padding = int(math.log10(len(tracklist))) + 1
+        print('Warning: \'{0}\' is missing tag {1}'.format(track.path, err))
+        print('Guessing number of leading zeros from tracklist')
+        # Fallback to length of the tracklist if 'TRACKTOTAL' is missing
+        padding = int(math.log10(tracklist_length)) + 1
 
-    for k, t in zip(keys, tagnames):
+    for key, tag in zip(keys, tagnames):
         try:
-            value = track.tags[t][0].replace('/', '-')
-            if t == "TRACKNUMBER":
+            value = track.tags[tag][0].replace('/', '-')
+            if tag == 'TRACKNUMBER':
                 value = value.zfill(padding)  # Add leading zero(s)
-            filename_map[k] = value
+            filename_map[key] = value
         except KeyError as err:
-            print("Warning: '{0}' is missing tag {1}".format(track.path, err))
-            filename_map[k] = ''
+            print('Warning: \'{0}\' is missing tag {1}'.format(track.path, err))
+            filename_map[key] = ''
     return filename_map
 
 
-def get_pattern(track):
-    if not args['--pattern']:
+def _get_pattern(track):
+    if not ARGS['--pattern']:
         try:
             if track.tags['DISCTOTAL'][0] == '1':
                 return '{N} - {T}'
-            else:
-                return '{D}-{N} - {T}'
+            return '{D}-{N} - {T}'
         except KeyError:
             return '{N} - {T}'
     else:
-        return args['--pattern']
+        return ARGS['--pattern']
 
 
-def yes_no(question):
+def _yes_no(question):
+    '''
+    Continuously asks a yes/no question until user input is in [yYnN].
+    Returns True or False respectively.
+    '''
     yesno_map = {'y': True, 'n': False}
     choice = ''
     while choice not in ['y', 'n']:
@@ -191,34 +219,60 @@ def yes_no(question):
     return yesno_map[choice]
 
 
-def open_files():
-    for filename in args['FILE']:
+def _open_files(filenames):
+    '''
+    Opens all files in the param filenames with taglib.
+    Exits if no files can be opened.
+    '''
+    filelist = []
+    for filename in filenames:
         filename = os.path.abspath(filename)
         try:
             track = taglib.File(filename)
-            tracklist.append(track)
+            filelist.append(track)
         except OSError:
-            print("Unable to open file '{0}'".format(filename))
+            print('Unable to open file \'{0}\''.format(filename))
     # Exit audiotag if no tracks could be opened
-    if not tracklist:
-        print("No files could be opened")
+    if not filelist:
+        print('No files could be opened')
         sys.exit(1)
-    return tracklist
+    return filelist
+
+
+def _list_files(directory):
+    '''
+    Returns a list of all the files in a given directory.
+    Exits if path does not exist.
+    '''
+    try:
+        directory = os.path.abspath(directory)
+        direntries = os.listdir(directory)
+    except OSError as err:
+        print(err)
+        sys.exit(1)
+    files = []
+    for filename in direntries:
+        filepath = "{0}/{1}".format(directory, filename)
+        if os.path.isfile(filepath) and not os.path.islink(filepath):
+            files.append(filepath)
+    return files
 
 
 def main():
-    open_files()
-    if args['print']:
+    '''The main function. Starts whatever mode the user specified.'''
+    if ARGS['print']:
         print_mode()
-    elif args['set']:
+    elif ARGS['set']:
         set_mode()
-    elif args['clean']:
+    elif ARGS['clean']:
         clean_mode()
-    elif args['interactive']:
+    elif ARGS['interactive']:
         interactive_mode()
-    elif args['rename']:
+    elif ARGS['rename']:
         rename_mode()
+    elif ARGS['copy']:
+        copy_mode()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
